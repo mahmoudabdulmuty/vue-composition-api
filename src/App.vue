103e1755232ef1ca27313e7af5f72aa56bb33641
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-06 11:17:05 +0200

    vmodel with compostion API

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,10 @@
 <template>
-  <div>
-    <h2>
-      {{ count }}
-    </h2>
-    <h2>
-      {{ firstName }}
-    </h2>
-    <h2>
-      {{ lastName }}
-    </h2>
-    <button @click="changeName">change Name</button>
-    <button @click="increment">Counter</button>
-  </div>
+  <new-file />
 </template>
 
 <script>
-import { reactive, ref, toRefs } from 'vue';
-
-export default {
-  setup() {
-    const count = ref(0);
-    const increment = () => {
-      count.value++;
-    };
-
-    const data = reactive({
-      firstName: 'Mahmoud',
-      lastName: 'Abdulmuty',
-    });
-
-    const changeName = () => {
-      (data.firstName = 'Moemen'), (data.lastName = 'Yasser');
-    };
-
-    return {
-      ...toRefs(data),
-      count,
-      increment,
-      changeName,
-    };
-  },
-};
+import NewFile from './components/new-file.vue';
+export default { components: { NewFile } };
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/new-file.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input placeholder="First Name" type="text" v-model="firstName" />
+    <input placeholder="Last Name" type="text" v-model="lastName" />
+    <input placeholder="Hero Name" type="text" v-model="heroName" />
+  </div>
+</template>
+
+<script>
+import { reactive, ref, toRefs } from 'vue';
+
+export default {
+  setup() {
+    const heroName = ref('');
+    const fullName = reactive({
+      firstName: '',
+      lastName: '',
+    });
+
+    return {
+      heroName,
+      ...toRefs(fullName),
+    };
+  },
+};
+</script>
+
+<style scoped></style>
