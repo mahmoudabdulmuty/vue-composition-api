433e0c268593d0327277210c7762a7caf8d15386
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-07 09:24:59 +0200

    Props and Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <new-file />
+  <div>
+    <input type="text" placeholder="first name" v-model="fName" />
+    <input type="text" placeholder="last name" v-model="lName" />
+    <ChildComponent :fName="fName" :lName="lName" />
+  </div>
 </template>
 
-<script>
-import NewFile from './components/new-file.vue';
-export default { components: { NewFile } };
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './components/child-component.vue';
+
+const fName = ref('');
+const lName = ref('');
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/child-component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Hello {{ fullName }}</div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  fName: String,
+  lName: String,
+});
+const fullName = computed(() => {
+  return `${props.fName} ${props.lName}`;
+});
+</script>
+
+<style lang="scss" scoped></style>
